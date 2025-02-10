cde1dfa14331563f1fe2dc3befce2800656166fe
lpjr03 <lpjr03@gmail.com>
2025-02-10 14:48:45 +0100

    changed visualization of ratio comments/total lines over time

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import numpy as np
 from git import Repo
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -5,18 +6,30 @@ import matplotlib.pyplot as plt
 repo = Repo("C:/Users/lpjr0/Desktop/vue")
 
 commits = sorted(repo.iter_commits(), key=lambda c: c.committed_date)
-
 commit_dates = [datetime.fromtimestamp(commit.committed_date) for commit in commits]
 
-file_path = "stats.txt"
-with open(file_path, "r") as file:
-    y_values = [float(line.strip()) for line in file]
+comment_lines = []
+total_lines = []
+
+comment_lines_file = "output.txt"
+with open(comment_lines_file, "r") as file:
+    for line in file:
+        parts = line.split(",")
+        comment_count = int(parts[0].split(":")[1].strip())
+        comment_lines.append(comment_count)
+        total_count = int(parts[1].split(":")[1].strip())
+        total_lines.append(total_count)
 
 plt.figure(figsize=(10, 6))
-plt.plot(commit_dates, y_values, marker="o", linestyle="-")
+
+plt.plot(commit_dates, comment_lines, marker="o", linestyle="-", label="Comment Lines", color="blue")
+plt.plot(commit_dates, total_lines, marker="o", linestyle="-", label="Total Lines", color="red")
+
 plt.xlabel("Date")
-plt.ylabel("Percentage")
-plt.title("Percentage Change of Commits during Time")
+plt.ylabel("Number of Lines")
+plt.title("Comment Lines vs Total Lines Over Time")
 plt.grid()
+plt.legend()
+
 plt.tight_layout()
 plt.show()
